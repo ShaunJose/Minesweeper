85565d761a959d62503158a2684dc8d1b9113533
ShaunJose <shaun.jose12@gmail.com>
2019-11-17 17:46:20 +0000

    Changed model of cell to display include Element in cell, and status of a cell

--- src/Main.hs
+++ src/Main.hs
@@ -2,28 +2,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
---imports
+-- imports
 import System.Random
 
 -- Modelling a cell in minesweeper --
-type RowNum = Int
-type ColNum = Int
-data Cell = Cell (RowNum, ColNum)
+data Element = Mine | Num Int -- Element = What's in the cell?
+                deriving Show
+data Status = Shown | Hidden | Flagged -- Status = status of cell
+                deriving Show
+type RowNum = Int -- rownumber (from 0 to m-1)
+type ColNum = Int -- columnNumber (from 0 to n-1)
+data Cell = Cell (RowNum, ColNum) Element Status  -- Cell definition --
               deriving Show
 
 -- Board is a list of rows, which is a list of cells
 type Row = [Cell]
 type Board = [[Cell]]
 
+
+-- create a row (a list of cells), intialised all to Num 0 and Hidden
 createRow :: RowNum -> ColNum -> ColNum -> Row
 createRow row n currCol | n == currCol = []
-createRow row n currCol = Cell (row, currCol) : createRow row n (currCol+1)
+createRow row n currCol = Cell (row, currCol) (Num 0) Hidden : createRow row n (currCol+1)
 
 -- [ [Cell (0, 0), Cell (0, 1), Cell (0, 2) Cell (0, 3)],
 --   [Cell (1, 0), Cell (1, 1), Cell (1, 2) Cell (1, 3)],
 --   [Cell (2, 0), Cell (2, 1), Cell (2, 2) Cell (2, 3)] ]
 
-createBoard rows cols | n < 0 = []
+-- createBoard rows cols | n < 0 = []
 -- createBoard n =
 
 -- initialise game --
